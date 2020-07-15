d00f2ab28e9a60ee7f8176854276918e2c4157d8
Harsh6879 <harshkumar2272001@gmail.com>
2020-07-15 23:20:18 +0530

    Updates

---
+++ precache-manifest.433b663937079d9f144f449fd039f902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba10b72a5e4af91f65d5c7040f180cd",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "e3baaad85e98c0f8fc2c",
+    "url": "/React/static/css/main.5e3e5441.chunk.css"
+  },
+  {
+    "revision": "ed0846d11552f1f8c234",
+    "url": "/React/static/js/2.0ffd0c5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.0ffd0c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3baaad85e98c0f8fc2c",
+    "url": "/React/static/js/main.7decf103.chunk.js"
+  },
+  {
+    "revision": "bbe529a00f504c8d636c",
+    "url": "/React/static/js/runtime-main.8170ce0e.js"
+  }
+]);
